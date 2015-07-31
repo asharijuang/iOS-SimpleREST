1a006d40be02e5fb055cde83b01d06ad77277630
asharijuang <asharijuang@me.com>
2015-07-31 09:32:30 +0700

    Initial Commit

---
+++ SimpleREST/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleREST
+//
+//  Created by Ashari Juang on 7/31/15.
+//  Copyright (c) 2015 KodeJS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleREST/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SimpleREST
+//
+//  Created by Ashari Juang on 7/31/15.
+//  Copyright (c) 2015 KodeJS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

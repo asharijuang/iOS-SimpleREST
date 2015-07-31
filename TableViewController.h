82698c304deaa309a6e14cff53176c28160e5a89
asharijuang <asharijuang@me.com>
2015-07-31 10:00:38 +0700

    Add table view controller and add ui table view on storyboard

---
+++ TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  SimpleREST
+//
+//  Created by Ashari Juang on 7/31/15.
+//  Copyright (c) 2015 KodeJS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
